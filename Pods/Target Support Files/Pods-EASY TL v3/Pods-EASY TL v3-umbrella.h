6a819a5ca5a998c6c9f37205d914c1bf72a4b95e
Daria Fedorenko <daria.fedorenko@nure.ua>
2019-12-02 16:04:49 +0200

    init

---
+++ Pods/Target Support Files/Pods-EASY TL v3/Pods-EASY TL v3-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_EASY_TL_v3VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EASY_TL_v3VersionString[];
+
